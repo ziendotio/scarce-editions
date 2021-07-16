e366dcc0a3b9b1aac660d373068325997acf308d
LDNCWRB <londoncodeworkswebdevprogramrb@outlook.com>
2021-07-16 10:35:42 +0100

    init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,7 +16,7 @@ import { HardhatUserConfig } from 'hardhat/types';
 // Go to https://hardhat.org/config/ to learn more
 
 // private key from the pre-funded Beresheet testing account
-const { privateKey } = require('./private.json');
+const { INFURA_API_KEY, ALCHEMY_API_KEY, BERESHEET_PRIVATE_KEY, ROPSTEN_PRIVATE_KEY, MAINNET_PRIVATE_KEY } = require('./private.json');
 
 const config: HardhatUserConfig = {
 	abiExporter: {
@@ -36,13 +36,23 @@ const config: HardhatUserConfig = {
 		},
 	},
 	networks: {
-		// Beresheet network specification
+		// Beresheet network specification // npx hardhat run scripts/deploy.js --network beresheet
 		Beresheet: {
 		  url: `https://beresheet2.edgewa.re/evm`,
 		  chainId: 2022,
-		  accounts: [privateKey]
-		}
-	  }
+		  accounts: [BERESHEET_PRIVATE_KEY]
+		},
+		// Ropsten network specification // npx hardhat run scripts/deploy.js --network ropsten
+		ropsten: {
+			url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+			accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+		  },
+		// Mainnet network specification // npx hardhat run scripts/deploy.js --network mainnet
+		mainnet: {
+			url: `https://mainnet.infura.io/v3/${ALCHEMY_API_KEY}`, // or any other JSON-RPC provider // <INFURA_API_KEY>
+			accounts: [MAINNET_PRIVATE_KEY]
+		} 
+	  },
 };
 
 export default config;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: MIT
 // scripts/deploy.js
+
 async function main() {
   // Deployer
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
-
 
   // We get the contract to deploy
   const ScarceEdition = await ethers.getContractFactory('ScarceEdition');
@@ -16,7 +17,6 @@ async function main() {
   // Waiting for the deployment to resolve
   await scarceEdition.deployed();
   console.log('ScarceEdition deployed to:', scarceEdition.address);
-  console.log('block:', scarceEdition);
 }
 
 main()
@@ -25,31 +25,3 @@ main()
      console.error(error);
      process.exit(1);
   });
-
-
-
-// // SPDX-License-Identifier: MIT
-
-// async function main() {
-
-//     const [deployer] = await ethers.getSigners();
-  
-//     console.log(
-//       "Deploying contracts with the account:",
-//       deployer.address
-//     );
-    
-//     console.log("Account balance:", (await deployer.getBalance()).toString());
-  
-//     const ScarceEdition = await ethers.getContractFactory("ScarceEdition");
-//     const scarceEdition = await ScarceEdition.deploy();
-  
-//     console.log("ScarceEdition address:", scarceEdition.address);
-//   }
-  
-//   main()
-//     .then(() => process.exit(0))
-//     .catch(error => {
-//       console.error(error);
-//       process.exit(1);
-//     });
